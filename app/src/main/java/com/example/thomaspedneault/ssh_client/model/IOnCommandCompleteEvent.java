9cc0edf07f3b2e10e7474dd92d3dfda0a64b5b65
Thomas Pedneault <thomas.pedneault@icloud.com>
2018-11-29 14:06:57 -0500

    Made enter key execute command
    Instead of having to press the send button to execute the command you can now just press enter while typing in the command EditText

--- app/src/main/java/com/example/thomaspedneault/ssh_client/model/IOnCommandCompleteEvent.java
+++ app/src/main/java/com/example/thomaspedneault/ssh_client/model/IOnCommandCompleteEvent.java
@@ -1,5 +1,7 @@
 package com.example.thomaspedneault.ssh_client.model;
 
 public interface IOnCommandCompleteEvent {
+
     void onComplete(String output);
+
 }
--- app/src/main/java/com/example/thomaspedneault/ssh_client/ui/TerminalFragment.java
+++ app/src/main/java/com/example/thomaspedneault/ssh_client/ui/TerminalFragment.java
@@ -1,8 +1,10 @@
 package com.example.thomaspedneault.ssh_client.ui;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,16 +20,19 @@ import com.example.thomaspedneault.ssh_client.model.ServerConnection;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class TerminalFragment extends Fragment {
 
+    private static final String OUTPUT_LINE_PREFIX = ">";
+
+    @SuppressLint("SimpleDateFormat")
     public static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("[HH:mm:ss]");
 
-    public TerminalFragment() {
-    }
+    public TerminalFragment() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,10 +47,25 @@ public class TerminalFragment extends Fragment {
 
             @Override
             public void onComplete(ServerConnection.States state) {
-                // Set the event listener on the button.
+                // Set the event listeners for the UI elements.
                 Button executeButton = root.findViewById(R.id.execute_Button);
+                EditText commandEditText = root.findViewById(R.id.command_EditText);
+
+                commandEditText.setOnKeyListener((v, keyCode, event) -> {
+                    if(event.getAction() == KeyEvent.ACTION_DOWN) {
+                        switch(keyCode) {
+                            case KeyEvent.KEYCODE_ENTER:
+                                executeButton.callOnClick();
+                                return true;
+                            default:
+                                break;
+                        }
+                    }
+
+                    return false;
+                });
+
                 executeButton.setOnClickListener(v -> {
-                    EditText commandEditText = root.findViewById(R.id.command_EditText);
                     EditText outputEditText = root.findViewById(R.id.output_EditText);
 
                     String command = commandEditText.getText().toString();
@@ -53,16 +73,22 @@ public class TerminalFragment extends Fragment {
                     // Close the keyboard.
                     InputMethodManager inputManager = (InputMethodManager)
                             getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
-                    inputManager.hideSoftInputFromWindow(
-                            getActivity().getCurrentFocus().getWindowToken(), 0);
+                    Objects.requireNonNull(inputManager).hideSoftInputFromWindow(
+                            Objects.requireNonNull(getActivity().getCurrentFocus()).getWindowToken(), 0);
+
+                    // Check if the command is to clear the console.
+                    if(command.equals("clear")) {
+                        outputEditText.setText("");
+                        return;
+                    }
 
                     connection.asyncExecCommand(command, output -> getActivity().runOnUiThread(() ->
                     {
                         String prefix = DATE_FORMAT.format(new Date()) + " " + connection.getIdentity().getUsername();
                         outputEditText.append(prefix + ": " + command + "\n");
                         String[] lines = output.split("\n");
-                        for(int i = 0; i < lines.length; i++) {
-                            outputEditText.append("-->" + lines[i] + "\n");
+                        for (String line : lines) {
+                            outputEditText.append(OUTPUT_LINE_PREFIX + line + "\n");
                         }
                     }));
                 });
